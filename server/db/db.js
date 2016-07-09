393ecda48f7a7f5ea1ca2de972e93a1f605fcb92
Phil Seidel <psbyron3@gmail.com>
2016-07-08 18:36:37 -0700

    [Feature] Created add game to add pickup event to db

--- server/db/db.js
+++ server/db/db.js
@@ -2,37 +2,75 @@
 //_____________________________MODULES_________________________________________
 var mysql = require('mysql');
 var fs = require('fs');
+var tables = require('./tables.js');
+var db_config = require('./dbConfig.js');
+
 
 
 //init connection from Poll_db
-var db = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'polls_db',
-  multipleStatements : true
-});
+var createConnection = function() {
+  connection = mysql.createConnection(db_config);
 
-//Error handle 
-db.on('error', function() { console.error("ERROR in database") });
-
-//Init DB setup when server boots
-fs.readFile(__dirname + '/polls.sql', 'utf-8', function(err, data) {
-  //Multiple statement work-around
-  var commands = data.split(";");
-  commands.pop(); 
-  commands.forEach (function(command) {
-    db.query(command, function(err, results) {
-      if (err) {
-        console.error(err);
-      } 
-    });
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err);
+    }
   });
-});
+
+  connection.on('error', function(err) {
+    console.error(err);
+    createConnection();
+  });
+}
+
+createConnection();
+
+tables.create();
+
+module.exports = {
+
+  //Error handle 
+  // createConnection.on('error', function() { console.error("ERROR in database") });
+
+  // //Init DB setup when server boots
+  // fs.readFile(__dirname + '/pickups.sql', 'utf-8', function(err, data) {
+  //   //Multiple statement work-around
+  //   var commands = data.split(";");
+  //   commands.pop(); 
+  //   commands.forEach (function(command) {
+  //     createConnection.query(command, function(err, results) {
+  //       if (err) {
+  //         console.error(err);
+  //       } 
+  //     });
+  //   });
+  // });
+  addGame : function(sport, rules, time, player_max, created_by, callback) {
+    var check = 'SELECT * FROM Games WHERE sport = ? AND time = ?';
+    var checkValues = [sport, time];
+    var insert = "INSERT into Games (sport, rules, time, player_max, created_by) values (?, ?, ?, ?, ?);";
+    var insertValues = [sport, rules, time, player_max, created_by];
 
 
-// 5-second keep-alive request
-setInterval(function () {db.query('SELECT 1') }, 5000);
+    connection.query(check, checkValues, function(err, data) {
+      if (err) {
+        console.error("error 1 db addGame : ", err);
+      }
+      if (data.length === 0) {
+        connection.query(insert, insertValues, function(err) {
+          if (err) {console.error('error 2 in db addGame :', err)}
+          else { callback(true); }
 
-//expose for server use
-module.exports = db;
+        })
+      } else {
+        callback(false);
+      }
+    });
+  },
+
+
+
+  // 5-second keep-alive request
+  setInterval(function () {db.query('SELECT 1') }, 5000);
+
+};
