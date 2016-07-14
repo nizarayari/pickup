4d4f79ad92d9a0de952f95eed403511b7c5cd5ef
mikeynova <newfoundland7@gmail.com>
2016-07-14 14:17:13 -0700

    [bug] deleted client folder

--- client/public/reducers/reducer_getGames.js
+++
@@ -1,9 +0,0 @@
-import { GET_GAMES } from '../actions/index';
-
-export default function(state = [], action) {
-  switch(action.type){
-    case GET_GAMES:
-      return action.payload;
-  }
-  return state;
-}
--- client/public/reducers/reducer_searchGames.js
+++
@@ -1,9 +0,0 @@
-import { SEARCH_GAMES } from '../actions/index';
-
-export default function(state = [], action) {
-  switch(action.type){
-    case SEARCH_GAMES:
-      return action.payload;
-  }
-  return state;
-}
